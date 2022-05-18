3d4cf8c64102d21444057e494af36489911c4a30
Malosdaf <bellmang2005@gmail.com>
2022-05-18 23:54:55 +0700

    Adding Timeline to Achievement

--- src/Achievement.svelte
+++ src/Achievement.svelte
@@ -1,6 +1,9 @@
 <script>
     import Card from "./Card.svelte";
 </script>
+
 <Card>
-    <h1> </h1>
+    <div class="p-8">
+        <img class="w-5/6 mx-auto" src="images/Timeline.svg" alt="Timelime" >
+    </div>
 </Card>
